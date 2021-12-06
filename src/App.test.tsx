735022fe41ac4070c6cdb45489670c419b2d32b1
David Lechner <david@ablerobots.com>
2021-12-06 01:11:41 +0000

    fix failing test with eventsourcemock
    Jest doesn't have EventSource, so we need a mock.
    
    eventsourcemock does the job but it chokes on null configuration provided by ReconnectingEventSource, so we have to patch it.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import EventSource from 'eventsourcemock';
+
+Object.defineProperty(window, 'EventSource', {
+  value: EventSource,
+});
 
 test("renders header", () => {
   render(<App />);
