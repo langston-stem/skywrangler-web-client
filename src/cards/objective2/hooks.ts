c2d8ea385955f37010d3f431b3b9862ae6b56b37
terrancewoodard <112575741+terrancewoodard@users.noreply.github.com>
2022-09-28 16:29:28 -0500

    Parameter input from slider
    This program takes the input from the slider and stores them for the drone.

---
+++ src/cards/objective2/hooks.ts
@@ -0,0 +1,49 @@
+import { useLocalStorage } from "usehooks-ts";
+
+export function useLatitude(): {
+  latitude: number;
+  setLatitude: (value: number) => void;
+} {
+  const [latitude, setLatitude] = useLocalStorage("objective2-latitude", 0);
+  return { latitude, setLatitude };
+}
+
+export function useLongitude(): {
+  longitude: number;
+  setLongitude: (value: number) => void;
+} {
+  const [longitude, setLongitude] = useLocalStorage("objective2-longitude", 0);
+  return { longitude, setLongitude };
+}
+
+export function useAngle(): {
+  angle: number;
+  setAngle: (value: number) => void;
+} {
+  const [angle, setAngle] = useLocalStorage("objective2-angle", 90);
+  return { angle, setAngle };
+}
+
+export function useElevation(): {
+  elevation: number;
+  setElevation: (value: number) => void;
+} {
+  const [elevation, setElevation] = useLocalStorage("objective2-elevation", 0);
+  return { elevation, setElevation };
+}
+
+export function useSpeed(): {
+  speed: number;
+  setSpeed: (value: number) => void;
+} {
+  const [speed, setSpeed] = useLocalStorage("objective2-speed", 2);
+  return { speed, setSpeed };
+}
+
+export function useDistance(): {
+  distance: number;
+  setDistance: (value: number) => void;
+} {
+  const [distance, setDistance] = useLocalStorage("objective2-distance", 30);
+  return { distance, setDistance };
+}
