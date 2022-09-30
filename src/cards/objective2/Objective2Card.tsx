9fe30351974c96d2bc9627005dd121f4ba8f053b
David Lechner <david@ablerobots.com>
2022-09-30 03:40:48 +0000

    Objective2Card: add origin lat/lon/elv inputs
    Fixes #4

--- src/cards/objective2/Objective2Card.tsx
+++ src/cards/objective2/Objective2Card.tsx
@@ -1,9 +1,30 @@
 import Card from "../Card";
 import * as React from "react";
-import { Slider, SliderTrack, SliderThumb, SliderMark } from "@chakra-ui/react";
+import {
+  Slider,
+  SliderTrack,
+  SliderThumb,
+  SliderMark,
+  Input,
+  SimpleGrid,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  Box,
+  AccordionIcon,
+  AccordionPanel,
+} from "@chakra-ui/react";
 import { Text, VStack } from "@chakra-ui/react";
-import { useAngle, useDistance, useSpeed } from "./hooks";
+import {
+  useAngle,
+  useDistance,
+  useElevation,
+  useLatitude,
+  useLongitude,
+  useSpeed,
+} from "./hooks";
 import { useCallback } from "react";
+import { useState } from "react";
 
 function fromDistance(distance: number): number {
   if (distance === 3) {
@@ -46,7 +67,48 @@ const labelStyles = {
 const langstonBlue = "rgb(27,54,104)";
 const langstonOrange = "rgb(242,104,42)";
 
+type NumberInputProps = {
+  value: number;
+  onChange: (value: number) => void;
+};
+
+const NumberInput: React.FunctionComponent<NumberInputProps> = ({
+  value,
+  onChange,
+}) => {
+  const [stringValue, setStringValue] = useState(String(value));
+  const [isValid, setIsValid] = useState(true);
+
+  const handleChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setStringValue(event.target.value);
+
+      const newValue = Number(event.target.value);
+
+      if (isNaN(newValue)) {
+        setIsValid(false);
+      } else {
+        setIsValid(true);
+        onChange(newValue);
+      }
+    },
+    [onChange]
+  );
+
+  return (
+    <Input
+      inputMode="decimal"
+      isInvalid={!isValid}
+      value={stringValue}
+      onChange={handleChange}
+    />
+  );
+};
+
 const Objective2Card: React.FunctionComponent = () => {
+  const { latitude, setLatitude } = useLatitude();
+  const { longitude, setLongitude } = useLongitude();
+  const { elevation, setElevation } = useElevation();
   const { angle, setAngle } = useAngle();
   const { distance, setDistance } = useDistance();
   const { speed, setSpeed } = useSpeed();
@@ -61,6 +123,34 @@ const Objective2Card: React.FunctionComponent = () => {
 
   return (
     <Card title="Objective 2">
+      <Accordion allowToggle>
+        <AccordionItem>
+          <AccordionButton>
+            <Box flex="1" textAlign="center">
+              Origin
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel pb={4}>
+            <SimpleGrid
+              columns={2}
+              spacing={1}
+              gridTemplateColumns="90px auto"
+              alignItems="center"
+              justifyItems="start"
+            >
+              <Text>Latitude</Text>
+              <NumberInput value={latitude} onChange={setLatitude} />
+
+              <Text>Longitude</Text>
+              <NumberInput value={longitude} onChange={setLongitude} />
+
+              <Text>Elevation</Text>
+              <NumberInput value={elevation} onChange={setElevation} />
+            </SimpleGrid>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
       <VStack paddingBottom={4}>
         <Text fontSize={"small"}>Angle</Text>
         <Slider value={angle} onChange={setAngle} min={30} max={90} step={30}>
