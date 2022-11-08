9f0c9f8b368d253dd725412928c9a2187b7e5834
terrancewoodard <112575741+terrancewoodard@users.noreply.github.com>
2022-11-08 17:19:02 -0600

    Text above azimuth arrow
    I updated the visualization and put text above the arrow of the azimuth

--- src/cards/objective2/Transect.tsx
+++ src/cards/objective2/Transect.tsx
@@ -17,15 +17,21 @@ export default function Transect() {
     }).appendTo(domElement.current);
 
     const transectLength = two.width * 0.7;
+    const azimuthLength = two.width * 0.7;
 
     var radius = 50;
 
     var rect = two.makeRectangle(0, 0, 100, 100);
     var circle = two.makeCircle(0, 0, 2);
 
-    const azimuth = two.makeArrow(0, 0, 0, -100, 10);
+    const azimuth = two.makeArrow(0, 0, 0, -110, 10);
     azimuth.stroke = "red";
     azimuth.linewidth = 3;
+
+    const azimuthLabel = two.makeText("Azimuth", -7, -73.5);
+    azimuthLabel.stroke = "red";
+    azimuthLabel.size = 11;
+    azimuthLabel.rotation = (270 * Math.PI) / 180;
 
     const transect = two.makeArrow(
       -transectLength / 2,
@@ -44,8 +50,15 @@ export default function Transect() {
       -57
     );
     transectLabel.stroke = "blue";
+    transectLabel.size = 12;
 
-    var group = two.makeGroup(rect, azimuth, transect, transectLabel);
+    var group = two.makeGroup(
+      rect,
+      azimuth,
+      transect,
+      transectLabel,
+      azimuthLabel
+    );
     group.position.set(two.width / 2, two.height / 2);
 
     var goat = two.makeSprite("goat.svg", two.width / 2, two.height / 2);
